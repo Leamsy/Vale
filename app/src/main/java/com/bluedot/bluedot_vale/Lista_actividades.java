80e4dd1dcc07c9510b861cae223c9f16cab21120
Leamsy <ismamoyano97@gmail.com>
2020-04-02 12:56:35 +0200

    Listaactividades firebase

--- app/src/main/java/com/bluedot/bluedot_vale/Lista_actividades.java
+++ app/src/main/java/com/bluedot/bluedot_vale/Lista_actividades.java
@@ -2,10 +2,15 @@ package com.bluedot.bluedot_vale;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
@@ -15,16 +20,31 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -35,14 +55,15 @@ import static android.view.View.INVISIBLE;
 
 public class Lista_actividades extends AppCompatActivity {
 
-    private String url = Global.web + "/wp-json/vale/v1/actividades/1";
-
     private List<ItemAdapter> data = new ArrayList<>();
     private ArrayAdapter<TextView> arrayAdapter;
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
     private RecyclerView.Adapter mAdapter;
     Context context = this;
+    String uid;
+
+    String imagen_url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +79,47 @@ public class Lista_actividades extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
 
 
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        CollectionReference colRef = FirebaseFirestore.getInstance().collection("actividades");
+
+        colRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+
+                    findViewById(R.id.gif).setVisibility(INVISIBLE);
+
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Log.d("aa", document.getId() + " => " + document.getData());
+
+                        final ItemAdapter itemAdapter = new ItemAdapter();
+                        itemAdapter.setText(document.getData().get("titulo").toString());
+
+                        imagen_url = document.getData().get("imagen").toString();
+                        itemAdapter.setImage(imagen_url);
+
+                        data.add(itemAdapter);
+                    }
+                    mAdapter = new MyAdapter(data, context);
+                    recyclerView.setAdapter(mAdapter);
+
+                    /*
+                    titulo = document.getData().get("titulo").toString();
+                    descripcion = document.getData().get("descripcion").toString();
+                    imagen = document.getData().get("imagen").toString();
+                    fecha = document.getData().get("fecha").toString();
+                    hora = document.getData().get("hora").toString();
+                    plazas_socios = document.getData().get("plazas_socios").toString();
+                    plazas_voluntarios = document.getData().get("plazas_voluntarios").toString();
+                    requiere_autorizacion = document.getData().get("requiere_autorizacion").toString();
+                    */
+                } else {
+                    Log.d("aa", "No existe el usuario");
+                }
+            }
+        });
+/*
         RequestQueue requestQueue = Volley.newRequestQueue( this );
 
         JsonArrayRequest objectRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
@@ -102,7 +164,7 @@ public class Lista_actividades extends AppCompatActivity {
             }
         };
 
-        requestQueue.add( objectRequest );
+        requestQueue.add( objectRequest );*/
     }
 
     public void volver(android.view.View V){
