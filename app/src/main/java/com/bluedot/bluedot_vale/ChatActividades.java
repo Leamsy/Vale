f19d56c0c5d9fd10c9aff094de8c9191bfc84ed5
manuuarizaa <manuariza95@gmail.com>
2020-04-17 14:28:44 +0200

    Falta parte firebase

--- app/src/main/java/com/bluedot/bluedot_vale/ChatActividades.java
+++ app/src/main/java/com/bluedot/bluedot_vale/ChatActividades.java
@@ -1,11 +1,14 @@
 package com.bluedot.bluedot_vale;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +18,9 @@ public class ChatActividades extends AppCompatActivity {
     private RecyclerView todosmensajes;
     private EditText mensaje;
     private ImageView enviar;
-    private List<MyAdapter_chat> listmensajes;
+    private List<Mensaje> listmensajes;
+
+    private MyAdapter_chat myAdapter_chat;
 
 
     @Override
@@ -28,6 +33,11 @@ public class ChatActividades extends AppCompatActivity {
         enviar = findViewById(R.id.enviarchat);
 
         listmensajes = new ArrayList<>();
+        myAdapter_chat = new MyAdapter_chat(listmensajes);
+        todosmensajes.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        todosmensajes.setAdapter(myAdapter_chat);
+        todosmensajes.setHasFixedSize(true);
+        
     }
 
 
