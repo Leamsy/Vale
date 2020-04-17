876561da1e4af74e9de4fd42e46eaffb38c7da63
manuuarizaa <manuariza95@gmail.com>
2020-04-17 20:10:15 +0200

    interfaz de chat terminada, falta que sea funcional

--- app/src/main/java/com/bluedot/bluedot_vale/ChatActividades.java
+++ app/src/main/java/com/bluedot/bluedot_vale/ChatActividades.java
@@ -1,32 +1,54 @@
 package com.bluedot.bluedot_vale;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ChatActividades extends AppCompatActivity {
+public class ChatActividades extends AppCompatActivity implements View.OnClickListener{
 
     private RecyclerView todosmensajes;
     private EditText mensaje;
     private ImageView enviar;
     private List<Mensaje> listmensajes;
+    private ImageView btnatras;
+    private String titulosalachat;
 
     private MyAdapter_chat myAdapter_chat;
+    String uid_act;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_actividades);
+
+        Intent intent = getIntent();
+        uid_act = intent.getStringExtra("uid");
+        btnatras = findViewById(R.id.atrasperfilchat);
+
+        btnatras.setOnClickListener(this);
 
         todosmensajes = findViewById(R.id.reciclerchat);
         mensaje = findViewById(R.id.campomensajes);
@@ -38,8 +60,40 @@ public class ChatActividades extends AppCompatActivity {
         todosmensajes.setAdapter(myAdapter_chat);
         todosmensajes.setHasFixedSize(true);
 
-        //FirebaseFirestore.getInstance().collection("chat").
+
+        //Obtener titulo de la actividad
+        DocumentReference docRef = FirebaseFirestore.getInstance().collection("actividades").document(uid_act);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                String TAG = "Sala chat";
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                       titulosalachat = document.getData().get("titulo").toString();
+                        TextView titulochat = findViewById(R.id.titulochat);
+                        titulochat.setText(titulosalachat);
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+
     }
 
+    public void volver(){
+        finish();
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.atrasperfilchat:
+                volver();
+                break;
+        }
+    }
 }
