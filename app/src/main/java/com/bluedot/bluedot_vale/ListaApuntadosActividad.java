9b19b5d51582fc97db189a45259ba3cd43a0a585
manuuarizaa <manuariza95@gmail.com>
2020-04-22 11:19:50 +0200

    Creados activity apuntados y rechazados y hecha estructura de vista de ambas

---
+++ app/src/main/java/com/bluedot/bluedot_vale/ListaApuntadosActividad.java
@@ -0,0 +1,14 @@
+package com.bluedot.bluedot_vale;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaApuntadosActividad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_apuntados_actividad);
+    }
+}
