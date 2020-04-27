69ba900a76c786ae0e39e50e7ed211b5eb68b86a
manuuarizaa <manuariza95@gmail.com>
2020-04-27 13:51:51 +0200

    Falta arreglar que borre el usuario que se elige

--- app/src/main/java/com/bluedot/bluedot_vale/MyAdapterApuntados.java
+++ app/src/main/java/com/bluedot/bluedot_vale/MyAdapterApuntados.java
@@ -2,13 +2,22 @@ package com.bluedot.bluedot_vale;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,7 +25,10 @@ import androidx.recyclerview.widget.RecyclerView;
 public class MyAdapterApuntados extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private List<ItemAdapter> mList;
     private Context mContext;
-
+    private String TAG = "Borrar usuario de apuntado";
+    private String esAutor;
+    private String idActividad;
+    private String idPersona;
 
     public MyAdapterApuntados(List<ItemAdapter> list, Context context){
         super();
@@ -33,6 +45,10 @@ public class MyAdapterApuntados extends RecyclerView.Adapter<RecyclerView.ViewHo
     @Override
     public void onBindViewHolder(@NonNull final RecyclerView.ViewHolder viewHolder, int position) {
         final ItemAdapter itemAdapter = mList.get(position);
+        esAutor = itemAdapter.getAutor();
+        idActividad = itemAdapter.getIdActividad();
+        idPersona = itemAdapter.getUidvisitante();
+
         ((ViewHolder) viewHolder).mTv_name.setText(itemAdapter.getText());
         ((ViewHolder) viewHolder).mTv_name.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +58,10 @@ public class MyAdapterApuntados extends RecyclerView.Adapter<RecyclerView.ViewHo
                 ((ViewHolder) viewHolder).context.startActivity(intent);
             }
         });
+
+        if(esAutor.equals("false"))
+        ((ViewHolder) viewHolder).btnEliminar.setVisibility(View.GONE);
+
         try {
             Picasso.get().load(itemAdapter.getImage()).into(((ViewHolder) viewHolder).mImg);
         } catch (Exception e) {
@@ -56,11 +76,19 @@ public class MyAdapterApuntados extends RecyclerView.Adapter<RecyclerView.ViewHo
             }
         });
 
+        ((ViewHolder) viewHolder).btnEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                borrarUsuarioDeApuntado();
+            }
+        });
+
     }
     @Override
     public int getItemCount() {
         return mList.size();
     }
+
     class ViewHolder extends RecyclerView.ViewHolder{
         public TextView mTv_name;
         public ImageView mImg;
@@ -75,5 +103,29 @@ public class MyAdapterApuntados extends RecyclerView.Adapter<RecyclerView.ViewHo
             btnEliminar = (ImageView) itemView.findViewById(R.id.btnBorrarUserActividad);
         }
     }
+
+    public void borrarUsuarioDeApuntado(){
+
+        //Esto borra el usuario que le da la gana (ARREGLARLO)
+        
+        FirebaseFirestore.getInstance().collection("actividades").document(idActividad).collection("apuntados").document(idPersona)
+                .delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully deleted!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "Error deleting document", e);
+                    }
+                });
+    }
+
+
+
+
 
 }
